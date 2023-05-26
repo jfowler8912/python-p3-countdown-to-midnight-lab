c06c14fb1fa04257681973ade98263ffbb4f9ebd
Joshua Fowler <jfowler8912@gmail.com>
2023-05-25 21:35:39 -0400

    initial commit

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,14 @@
-# your code goes here!
+import time
+def countdown(number = 10):
+    while number > 0:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+    print( "HAPPY NEW YEAR!")
+
+
+def countdown_with_sleep(number):
+    time.sleep(1)
+    while number > 0:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+    print( "HAPPY NEW YEAR!")
